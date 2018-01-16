ee9b69a41a3f85661229d048e3c309cbb95ae6a3
Vineelaj72 <35388524+Vineelaj72@users.noreply.github.com>
2018-01-15 22:31:22 -0600

    Updated Code

--- zeomega/src/main/java/com/jiva/setup/Setup.java
+++ zeomega/src/main/java/com/jiva/setup/Setup.java
@@ -3,7 +3,8 @@ package com.jiva.setup;
 public interface Setup {
 	
 	String AutomationURL="https://lumerisuat.zeomega.com";
-	String IEPATH = "../zeomega/drivers/IEDriverServer.exe"; 
+	String IEPATH = "../\zeomega/drivers/IEDriverServer.exe";
+	//String IEPATH = "C:\\Users\\vjayavarapu\\workspace\\JivaAutomation\\drivers\\IEDriverServer.exe";
 	String CHROMEPATH = "C:\\Softwares\\chromedriver_win32\\chromedriver.exe";
 	String FFPATH = "C:\\Softwares\\geckodriver-v0.19.1-win64\\geckodriver.exe";
 	
